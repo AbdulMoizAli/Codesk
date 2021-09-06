7793108c68954400f0ff17136fd7d8c65916b424
AbdulMoizAli <moizali_@outlook.com>
2021-09-07 03:20:24 +0500

    Add project files.

---
+++ CodeskWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CodeskWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
