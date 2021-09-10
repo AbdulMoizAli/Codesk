776e7749ef46bc9c116f899e28aa959538f55950
AbdulMoizAli <moizali_@outlook.com>
2021-09-10 21:09:45 +0500

    add connection class

---
+++ CodeskLibrary/Connections/DbConnection.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeskLibrary.Connections
+{
+    public static class DbConnection
+    {
+        public static SqlConnection GetConnection()
+        {
+            return new(GetConnectionString());
+        }
+
+        private static string GetConnectionString()
+        {
+            var config = new ConfigurationBuilder()
+                .SetBasePath(AppContext.BaseDirectory)
+                .AddJsonFile("appsettings.Development.json").Build();
+
+            return config.GetConnectionString("Codeskdb");
+        }
+    }
+}
