b2fe14c6293c8ddc9f2438b359eef06dc15c7545
AbdulMoizAli <moizali_@outlook.com>
2021-09-11 13:08:45 +0500

    check for empty strings

--- CodeskWeb/Areas/Users/Models/SignUpViewModel.cs
+++ CodeskWeb/Areas/Users/Models/SignUpViewModel.cs
@@ -5,23 +5,23 @@ namespace CodeskWeb.Areas.Users.Models
 {
     public class SignUpViewModel
     {
-        [Required(ErrorMessage = "Please enter your first name")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Please enter your first name")]
         [RegularExpression("^[a-zA-Z]+$", ErrorMessage = "First name can only have letters")]
         [StringLength(50, MinimumLength = 2, ErrorMessage = "Minimum 2 letters, Maximum 50 letters")]
         public string FirstName { get; set; }
 
-        [Required(ErrorMessage = "Please enter your last name")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Please enter your last name")]
         [RegularExpression("^[a-zA-Z]+$", ErrorMessage = "Last name can only have letters")]
         [StringLength(50, MinimumLength = 2, ErrorMessage = "Minimum 2 letters, Maximum 50 letters")]
         public string LastName { get; set; }
 
-        [Required(ErrorMessage = "Please enter a user name")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Please enter a user name")]
         [RegularExpression("^[a-zA-Z0-9_.-]+$", ErrorMessage = "User name can only have letters, digits, '_', '.' and '-'")]
         [StringLength(50, MinimumLength = 5, ErrorMessage = "Minimum 5 characters, Maximum 50 characters")]
         [Remote("ValidateUserName", "Account", "Users", ErrorMessage = "Username is already taken")]
         public string UserName { get; set; }
 
-        [Required(ErrorMessage = "Please enter an email address")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Please enter an email address")]
         [EmailAddress(ErrorMessage = "Email address is not valid")]
         [MaxLength(256, ErrorMessage = "Email address is too long, minimum 256 characters")]
         [Remote("ValidateEmailAddress", "Account", "Users", ErrorMessage = "Email address is already taken")]
@@ -31,7 +31,7 @@ namespace CodeskWeb.Areas.Users.Models
         [DataType(DataType.EmailAddress)]
         public string ConfirmEmailAddress { get; set; }
 
-        [Required(ErrorMessage = "Please enter a password")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Please enter a password")]
         [StringLength(100, MinimumLength = 8, ErrorMessage = "Minimum 8 characters, Maximum 100 characters")]
         [DataType(DataType.Password)]
         public string Password { get; set; }
