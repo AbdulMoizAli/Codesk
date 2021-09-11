c7ed1feda62b4d032fa40c3511466771ffe8bbcb
AbdulMoizAli <moizali_@outlook.com>
2021-09-11 13:03:08 +0500

    configure button disable on form submit and material dialog alert function

--- CodeskWeb/wwwroot/js/site.js
+++ CodeskWeb/wwwroot/js/site.js
@@ -1,3 +1,24 @@
 ﻿$(document).ready(() => {
     $('.tooltipped').tooltip();
+
+    $('form').submit(function (e) {
+        const form = $(this);
+
+        if (form.valid()) {
+            const submitBtn = form.find('button[type=submit]');
+            submitBtn.children(':first-child').attr('class', 'fas fa-sync fa-spin');
+            submitBtn.attr('disabled', true);
+        }
+    });
 });
+
+function showAlert(title = null, message, dark, okText) {
+    new duDialog(
+        title,
+        message,
+        {
+            dark,
+            okText
+        }
+    );
+}
