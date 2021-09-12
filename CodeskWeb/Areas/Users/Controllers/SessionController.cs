42819745833a3d19bf1ac25687b7137bb6aa7392
AbdulMoizAli <moizali_@outlook.com>
2021-09-12 21:51:19 +0500

    add SessionController

---
+++ CodeskWeb/Areas/Users/Controllers/SessionController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CodeskWeb.Areas.Users.Controllers
+{
+    public class SessionController : Controller
+    {
+        public IActionResult Dashboard()
+        {
+            return View();
+        }
+
+        public IActionResult NewSession()
+        {
+            return View();
+        }
+
+        [AllowAnonymous]
+        public IActionResult JoinSession()
+        {
+            return View();
+        }
+    }
+}
