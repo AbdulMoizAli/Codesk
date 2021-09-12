9b3be147edad09d5b0523c6a72799698aca52075
AbdulMoizAli <moizali_@outlook.com>
2021-09-12 21:54:32 +0500

    change site.js

--- CodeskWeb/wwwroot/js/site.js
+++ CodeskWeb/wwwroot/js/site.js
@@ -1,15 +1,6 @@
 ﻿$(document).ready(() => {
     $('.tooltipped').tooltip();
-
-    $('form').submit(function (e) {
-        const form = $(this);
-
-        if (form.valid()) {
-            const submitBtn = form.find('button[type=submit]');
-            submitBtn.children(':first-child').attr('class', 'fas fa-sync fa-spin');
-            submitBtn.attr('disabled', true);
-        }
-    });
+    $('.dropdown-trigger').dropdown();
 });
 
 function showAlert(title = null, message, dark, okText) {
