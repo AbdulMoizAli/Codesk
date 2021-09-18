71d590384bd848f305a0cee1b94ae44cf528ca74
AbdulMoizAli <moizali_@outlook.com>
2021-09-19 00:01:29 +0500

    add emailConfirmation.js

---
+++ CodeskWeb/wwwroot/js/Account/emailConfirmation.js
@@ -0,0 +1,20 @@
+﻿$(document).ready(() => {
+    $('#loader').hide();
+
+    $('#send-email').click(() => {
+        const email = $('#user-email').attr('data-userEmail');
+        showActionAlert('Confirm', `Activation link will be sent to "${email}"`, false, 'Cancel', 'Proceed', async () => {
+            M.toast({ html: 'Sending email...' });
+            $('#loader').show();
+
+            const response = await fetch('/Users/Email/SendAccountConfirmationEmail');
+
+            if (response.status === 200)
+                M.toast({ html: 'Email sent' });
+            else
+                M.toast({ html: 'Something went wrong while sending email' });
+
+            $('#loader').hide();
+        });
+    });
+});
