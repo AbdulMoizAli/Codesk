fbb5899460e781500e7e784472aa1da2f29ae362
AbdulMoizAli <moizali_@outlook.com>
2021-09-19 00:06:20 +0500

    add session controller in workspace area

---
+++ CodeskWeb/Areas/WorkSpace/Controllers/SessionController.cs
@@ -0,0 +1,34 @@
+﻿using CodeskLibrary.DataAccess;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace CodeskWeb.Areas.WorkSpace.Controllers
+{
+    public class SessionController : Controller
+    {
+        public IActionResult Dashboard()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> NewSession()
+        {
+            var email = User.FindFirst(x => x.Type == ClaimTypes.Email).Value;
+
+            var result = await EmailManager.IsEmailConfirmed(email).ConfigureAwait(false);
+
+            if (result)
+                return View();
+
+            return RedirectToAction("Dashboard", "Session", new { Area = "WorkSpace" });
+        }
+
+        [AllowAnonymous]
+        public IActionResult JoinSession()
+        {
+            return View();
+        }
+    }
+}
