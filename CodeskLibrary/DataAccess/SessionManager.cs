8f8409abbfe133603391baed82c70b98d55c1d45
AbdulMoizAli <moizali_@outlook.com>
2021-10-02 00:58:47 +0500

    add SessionManager.cs

---
+++ CodeskLibrary/DataAccess/SessionManager.cs
@@ -0,0 +1,31 @@
+﻿using CodeskLibrary.Connections;
+using CodeskLibrary.Models;
+using Dapper;
+using System.Collections.Generic;
+using System.Data;
+using System.Threading.Tasks;
+
+namespace CodeskLibrary.DataAccess
+{
+    public static class SessionManager
+    {
+        public static async Task<(IEnumerable<EditorSettingValues>, IEnumerable<EditorTheme>)> GetEditorSettings()
+        {
+            using IDbConnection db = DbConnection.GetConnection();
+
+            using var reader = await db.QueryMultipleAsync("spGetEditorSettings", commandType: CommandType.StoredProcedure)
+                .ConfigureAwait(false);
+
+            var editorSettings = reader.Read<EditorSettingValues, EditorSetting, EditorSettingValues>(
+                (value, setting) =>
+                {
+                    value.Setting = setting;
+                    return value;
+                }, splitOn: "SettingId");
+
+            var editorThemes = reader.Read<EditorTheme>();
+
+            return (editorSettings, editorThemes);
+        }
+    }
+}
