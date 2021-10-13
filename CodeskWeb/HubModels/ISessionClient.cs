673331a13c2ecf4d2066a204bd4c524f7b366c87
AbdulMoizAli <moizali_@outlook.com>
2021-10-13 21:44:37 +0500

    add hub models

---
+++ CodeskWeb/HubModels/ISessionClient.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace CodeskWeb.Hubs
+{
+    public interface ISessionClient
+    {
+        Task ReceiveNewSessionInfo(string userName, string sessionKey);
+
+        Task ReceiveJoinSessionInfo(List<string> userNames);
+
+        Task AddNewUserName(string userName);
+
+        Task NotifyUser(string notification);
+    }
+}
---
+++ CodeskWeb/HubModels/SessionInformation.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace CodeskWeb.HubModels
+{
+    public static class SessionInformation
+    {
+        public static Dictionary<string, List<string>> SessionInfo { get; set; } = new Dictionary<string, List<string>>();
+    }
+}
