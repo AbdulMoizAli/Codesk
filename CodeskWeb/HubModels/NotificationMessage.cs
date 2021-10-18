f2ab0b8802cd13606848186efae66b0d79e40c9b
AbdulMoizAli <moizali_@outlook.com>
2021-10-18 13:05:51 +0500

    add NotificationMessage.cs

---
+++ CodeskWeb/HubModels/NotificationMessage.cs
@@ -0,0 +1,20 @@
+﻿namespace CodeskWeb.HubModels
+{
+    public static class NotificationMessage
+    {
+        public static string GetWelcomeMessage(string userName)
+        {
+            return $"<span>Welcome to CODESK <span class=\"yellow-text\"> {userName}</span></span>";
+        }
+
+        public static string GetUserJoinMessage(string userName)
+        {
+            return $"<span><span class=\"yellow-text\"> {userName}</span> just joined the session</span>";
+        }
+
+        public static string GetUserLeaveMessage(string userName)
+        {
+            return $"<span><span class=\"yellow-text\"> {userName}</span> has left the session</span>";
+        }
+    }
+}
