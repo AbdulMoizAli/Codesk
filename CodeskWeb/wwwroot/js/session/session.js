f2e56ee662a5f9d2355983f90e554d64cb68633d
AbdulMoizAli <moizali_@outlook.com>
2021-10-21 22:57:37 +0500

    add updateParticipantCount function

--- CodeskWeb/wwwroot/js/session/session.js
+++ CodeskWeb/wwwroot/js/session/session.js
@@ -1,94 +1,100 @@
-﻿$(document).ready(() => {
-    (async () => {
-        $('#video-panel').sidenav({ edge: 'right' });
+﻿$(document).ready(async () => {
+    $('#video-panel').sidenav({ edge: 'right' });
 
-        window.addEventListener('beforeunload', e => {
-            e.preventDefault();
-            e.returnValue = '';
-        });
+    window.addEventListener('beforeunload', e => {
+        e.preventDefault();
+        e.returnValue = '';
+    });
 
-        $('#copy-session-key').click(() => {
-            navigator.clipboard.writeText($('#session-key-chip').text());
-            M.toast({ html: 'Copied to clipboard' });
-        });
+    $('#copy-session-key').click(() => {
+        navigator.clipboard.writeText($('#session-key-chip').text());
+        M.toast({ html: 'Copied to clipboard' });
+    });
 
-        $('#participant-search').keyup(function () {
-            const query = $(this).val().trim().toLowerCase();
+    $('#participant-search').keyup(function () {
+        const query = $(this).val().trim().toLowerCase();
 
-            if (!query) {
-                showUsers(sessionUsers);
-                return;
-            }
-
-            const filteredUsers = sessionUsers.filter(user => user.userName.toLowerCase().includes(query));
-
-            showUsers(filteredUsers);
-        });
-
-        function showUsers(users) {
-            const $list = $('.participant-list ul');
-
-            if (users.length === 0) {
-                $list.html('<li class="participant center"><a class="blue-grey-text">NO PARTICIPANT</a></li>');
-                return;
-            }
-
-            let markup = '';
-
-            for (const user of users) {
-                markup += `<li class="participant"><a class="blue-text text-darken-4" data-userid="${user.userId}">${user.userName.toUpperCase()}</a></li>`;
-            }
-
-            $list.html(markup);
+        if (!query) {
+            showUsers(sessionUsers);
+            return;
         }
 
-        function addUser(user) {
-            sessionUsers.push(user);
-            $('.participant-list ul').append(`<li class="participant"><a class="blue-text text-darken-4" data-userid="${user.userId}">${user.userName.toUpperCase()}</a></li>`);
+        const filteredUsers = sessionUsers.filter(user => user.userName.toLowerCase().includes(query));
+
+        showUsers(filteredUsers);
+    });
+
+    function showUsers(users) {
+        const $list = $('.participant-list ul');
+
+        if (users.length === 0) {
+            $list.html('<li class="participant center"><a class="blue-grey-text">NO PARTICIPANT</a></li>');
+            return;
         }
 
-        function removeUser(user) {
-            sessionUsers = sessionUsers.filter(x => x.userId !== user.userId);
-            $('.participant-list ul').find(`li a[data-userid="${user.userId}"]`).parent().remove();
+        let markup = '';
+
+        for (const user of users) {
+            markup += `<li class="participant"><a class="blue-text text-darken-4" data-userid="${user.userId}">${user.userName.toUpperCase()}</a></li>`;
         }
 
-        hubConnection.on('ReceiveNewSessionInfo', (user, _sessionKey) => {
-            sessionKey = _sessionKey;
-            addUser(user);
+        $list.html(markup);
+    }
 
-            $('#session-key-chip').text(_sessionKey);
-        });
+    function addUser(user) {
+        sessionUsers.push(user);
+        $('.participant-list ul').append(`<li class="participant"><a class="blue-text text-darken-4" data-userid="${user.userId}">${user.userName.toUpperCase()}</a></li>`);
+    }
 
-        hubConnection.on('ReceiveJoinSessionInfo', users => {
-            sessionUsers = users;
-            showUsers(users);
-        });
+    function removeUser(user) {
+        sessionUsers = sessionUsers.filter(x => x.userId !== user.userId);
+        $('.participant-list ul').find(`li a[data-userid="${user.userId}"]`).parent().remove();
+    }
 
-        hubConnection.on('AddUser', user => {
-            addUser(user);
-        });
+    function updateParticipantCount() {
+        $('#no-of-participants').text(sessionUsers.length);
+    }
 
-        hubConnection.on('RemoveUser', user => {
-            removeUser(user);
-        });
+    hubConnection.on('ReceiveNewSessionInfo', (user, _sessionKey) => {
+        sessionKey = _sessionKey;
+        addUser(user);
+        updateParticipantCount();
 
-        hubConnection.on('NotifyUser', notification => {
-            M.toast({ html: notification });
-        });
+        $('#session-key-chip').text(_sessionKey);
+    });
 
-        await hubConnection.start();
+    hubConnection.on('ReceiveJoinSessionInfo', users => {
+        sessionUsers = users;
+        showUsers(users);
+        updateParticipantCount();
+    });
 
-        if ($('#session-type').val() === 'new') {
-            await hubConnection.invoke('CreateSession');
-        }
-        else if ($('#session-type').val() === 'join') {
-            const userName = $('#session-username').val();
-            const _sessionKey = $('#session-key').val();
+    hubConnection.on('AddUser', user => {
+        addUser(user);
+        updateParticipantCount();
+    });
 
-            sessionKey = _sessionKey;
-            $('#session-key-chip').text(_sessionKey);
+    hubConnection.on('RemoveUser', user => {
+        removeUser(user);
+        updateParticipantCount();
+    });
 
-            await hubConnection.invoke('JoinSession', userName, _sessionKey);
-        }
-    })();
+    hubConnection.on('NotifyUser', notification => {
+        M.toast({ html: notification });
+    });
+
+    await hubConnection.start();
+
+    if ($('#session-type').val() === 'new') {
+        await hubConnection.invoke('CreateSession');
+    }
+    else if ($('#session-type').val() === 'join') {
+        const userName = $('#session-username').val();
+        const _sessionKey = $('#session-key').val();
+
+        sessionKey = _sessionKey;
+        $('#session-key-chip').text(_sessionKey);
+
+        await hubConnection.invoke('JoinSession', userName, _sessionKey);
+    }
 });
