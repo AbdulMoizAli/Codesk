057a52f706805f9044184827149c18f783084334
AbdulMoizAli <moizali_@outlook.com>
2021-11-05 22:48:17 +0500

    add videoChat.js

---
+++ CodeskWeb/wwwroot/js/session/videoChat.js
@@ -0,0 +1,25 @@
+﻿$(document).ready(() => {
+    const videoList = document.querySelector('.video-list ul');
+
+    navigator.mediaDevices.getUserMedia({
+        video: true,
+        audio: true
+    }).then(stream => {
+        const li = document.createElement('li');
+        li.className = 'video-item';
+
+        const div = document.createElement('div');
+        div.className = 'video-box';
+
+        const video = document.createElement('video');
+
+        video.muted = true;
+        video.srcObject = stream;
+        video.addEventListener('loadedmetadata', () => video.play());
+
+        div.append(video);
+        li.append(div);
+
+        videoList.append(li);
+    });
+});
