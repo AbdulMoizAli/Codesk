375d6b1efc2d768109724baf626c51b9e3684257
AbdulMoizAli <moizali_@outlook.com>
2021-11-19 03:04:05 +0500

    implement video calling using peerjs

--- CodeskWeb/wwwroot/js/session/videoChat.js
+++ CodeskWeb/wwwroot/js/session/videoChat.js
@@ -1,25 +1,74 @@
 ﻿$(document).ready(() => {
     const videoList = document.querySelector('.video-list ul');
+    const peers = {};
+    const peer = new Peer();
 
     navigator.mediaDevices.getUserMedia({
         video: true,
         audio: true
     }).then(stream => {
-        const li = document.createElement('li');
-        li.className = 'video-item';
-
-        const div = document.createElement('div');
-        div.className = 'video-box';
-
-        const video = document.createElement('video');
-
+        const { li, div, video } = getVideoElements();
         video.muted = true;
+
+        addVideoStream(li, div, video, stream);
+
+        peer.on('call', call => {
+            call.answer(stream);
+            const { li, div, video } = getVideoElements();
+            call.on('stream', userStream => addVideoStream(li, div, video, userStream));
+            call.on('close', () => video.parentElement.parentElement.remove());
+
+            peer.on('connection', peerConnection => {
+                peerConnection.on('data', userId => peers[userId] = call);
+            });
+        });
+        
+        hubConnection.on('ReceivePeerId', (peerId, userId) => connectToNewPeer(peerId, stream, userId));
+    });
+
+    hubConnection.on('CloseVideoCall', userId => {
+        if (peers[userId]) {
+            peers[userId].close();
+            delete peers[userId];
+        }
+    });
+
+    peer.on('open', async peerId => await hubConnection.invoke('SendPeerId', peerId, sessionKey));
+
+    function connectToNewPeer(peerId, stream, userId) {
+        const call = peer.call(peerId, stream);
+
+        const { li, div, video } = getVideoElements();
+
+        call.on('stream', userStream => addVideoStream(li, div, video, userStream));
+        call.on('close', () => video.parentElement.parentElement.remove());
+
+        const peerConnection = peer.connect(peerId);
+
+        peerConnection.on('open', () => {
+            peerConnection.send(sessionUsers[0].userId);
+        });
+
+        peers[userId] = call;
+    }
+
+    function getVideoElements() {
+        return {
+            li: document.createElement('li'),
+            div: document.createElement('div'),
+            video: document.createElement('video')
+        };
+    }
+
+    function addVideoStream(li, div, video, stream) {
         video.srcObject = stream;
         video.addEventListener('loadedmetadata', () => video.play());
 
+        li.className = 'video-item';
+        div.className = 'video-box';
+
         div.append(video);
         li.append(div);
-
         videoList.append(li);
-    });
+    }
 });
