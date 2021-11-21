713900772fa04b59c10271dbf97b2a5a216094af
AbdulMoizAli <moizali_@outlook.com>
2021-11-21 19:00:46 +0500

    fix namespace

--- CodeskWeb/HubModels/ISessionClient.cs
+++ CodeskWeb/HubModels/ISessionClient.cs
@@ -1,8 +1,7 @@
-﻿using CodeskWeb.HubModels;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Threading.Tasks;
 
-namespace CodeskWeb.Hubs
+namespace CodeskWeb.HubModels
 {
     public interface ISessionClient
     {
