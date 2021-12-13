c073093f19601558a83b1b1c88a4fb03669fd2b2
AbdulMoizAli <moizali_@outlook.com>
2021-12-14 00:06:27 +0500

    change peer configuration

--- CodeskWeb/wwwroot/js/session/videoChat.js
+++ CodeskWeb/wwwroot/js/session/videoChat.js
@@ -1,51 +1,14 @@
 ﻿export default function initializeVideoChat() {
     const videoList = document.querySelector('.video-list ul');
     const peers = {};
-    const peer = new Peer(undefined, {
-        config: {
-            iceServers: [
-                { url: 'stun:stun01.sipphone.com' },
-                { url: 'stun:stun.ekiga.net' },
-                { url: 'stun:stun.fwdnet.net' },
-                { url: 'stun:stun.ideasip.com' },
-                { url: 'stun:stun.iptel.org' },
-                { url: 'stun:stun.rixtelecom.se' },
-                { url: 'stun:stun.schlund.de' },
-                { url: 'stun:stun.l.google.com:19302' },
-                { url: 'stun:stun1.l.google.com:19302' },
-                { url: 'stun:stun2.l.google.com:19302' },
-                { url: 'stun:stun3.l.google.com:19302' },
-                { url: 'stun:stun4.l.google.com:19302' },
-                { url: 'stun:stunserver.org' },
-                { url: 'stun:stun.softjoys.com' },
-                { url: 'stun:stun.voiparound.com' },
-                { url: 'stun:stun.voipbuster.com' },
-                { url: 'stun:stun.voipstunt.com' },
-                { url: 'stun:stun.voxgratia.org' },
-                { url: 'stun:stun.xten.com' },
-                {
-                    url: 'turn:numb.viagenie.ca',
-                    credential: 'muazkh',
-                    username: 'webrtc@live.com'
-                },
-                {
-                    url: 'turn:192.158.29.39:3478?transport=udp',
-                    credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
-                    username: '28224511:1379330808'
-                },
-                {
-                    url: 'turn:192.158.29.39:3478?transport=tcp',
-                    credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
-                    username: '28224511:1379330808'
-                }
-            ]
-        }
-    });
+    const peer = new Peer();
+
+    let selfPeerId = undefined;
 
     navigator.mediaDevices.getUserMedia({
         video: true,
         audio: true
-    }).then(stream => {
+    }).then(async stream => {
         toggleVideoStream(stream);
         toggleAudioStream(stream);
 
@@ -68,6 +31,9 @@
         hubConnection.on('ReceivePeerId', (peerId, userId) => connectToNewPeer(peerId, stream, userId));
 
         controlMediaStream(stream);
+
+        if (selfPeerId)
+            await hubConnection.invoke('SendPeerId', selfPeerId, sessionKey);
     });
 
     hubConnection.on('CloseVideoCall', userId => {
@@ -77,7 +43,7 @@
         }
     });
 
-    peer.on('open', async peerId => await hubConnection.invoke('SendPeerId', peerId, sessionKey));
+    peer.on('open', peerId => selfPeerId = peerId);
 
     function connectToNewPeer(peerId, stream, userId) {
         const call = peer.call(peerId, stream);
