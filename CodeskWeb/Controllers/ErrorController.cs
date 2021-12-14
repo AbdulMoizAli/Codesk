2277f9ca119abf2a65794cdba4d3cfc8ceceb23f
AbdulMoizAli <moizali_@outlook.com>
2021-12-15 01:21:27 +0500

    add ErrorController.cs

---
+++ CodeskWeb/Controllers/ErrorController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CodeskWeb.Controllers
+{
+    [AllowAnonymous]
+    public class ErrorController : Controller
+    {
+        public IActionResult InternalServerError()
+        {
+            return View();
+        }
+
+        public IActionResult PageNotFound()
+        {
+            return View();
+        }
+    }
+}
