ca545929ec5f7748902fe7b64768c247d9a36d7f
AbdulMoizAli <moizali_@outlook.com>
2021-12-21 13:05:15 +0500

    add EditorController.cs

---
+++ CodeskWeb/Areas/WorkSpace/Controllers/EditorController.cs
@@ -0,0 +1,20 @@
+﻿using CodeskLibrary.DataAccess;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace CodeskWeb.Areas.WorkSpace.Controllers
+{
+    public class EditorController : Controller
+    {
+        [HttpPost]
+        public async Task<IActionResult> SaveUserEditorSetting(int settingId, string settingValue)
+        {
+            var email = User.FindFirst(x => x.Type == ClaimTypes.Email).Value;
+
+            await EditorManager.SaveUserEditorSetting(email, settingId, settingValue).ConfigureAwait(false);
+
+            return Ok();
+        }
+    }
+}
