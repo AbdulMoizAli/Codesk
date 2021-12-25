cdd0245bb91438820116ea6363bdefdf67d4ba4a
AbdulMoizAli <moizali_@outlook.com>
2021-12-25 14:46:34 +0500

    add SessionFile.cs

---
+++ CodeskLibrary/Models/SessionFile.cs
@@ -0,0 +1,15 @@
+﻿namespace CodeskLibrary.Models
+{
+    public class SessionFile
+    {
+        public int FileId { get; set; }
+
+        public int SessionId { get; set; }
+
+        public string FilePath { get; set; }
+
+        public int FileTypeId { get; set; }
+
+        public string FileTitle { get; set; }
+    }
+}
