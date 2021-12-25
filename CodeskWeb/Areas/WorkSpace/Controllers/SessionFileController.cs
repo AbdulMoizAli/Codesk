aa6dd41311a82922aa2faa552a6abda769e10a1f
AbdulMoizAli <moizali_@outlook.com>
2021-12-25 14:49:11 +0500

    add SessionFileController.cs

---
+++ CodeskWeb/Areas/WorkSpace/Controllers/SessionFileController.cs
@@ -0,0 +1,17 @@
+﻿using CodeskLibrary.DataAccess;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace CodeskWeb.Areas.WorkSpace.Controllers
+{
+    public class SessionFileController : Controller
+    {
+        [HttpPost]
+        public async Task<IActionResult> UpdateFileTitle(int fileId, string fileTitle)
+        {
+            await SessionFileManager.UpdateFileTitle(fileId, fileTitle);
+
+            return Ok();
+        }
+    }
+}
