51732da6350ce1f88e03b720935a377c1516d2c1
AbdulMoizAli <moizali_@outlook.com>
2021-12-25 14:46:17 +0500

    add FileType.cs

---
+++ CodeskLibrary/Models/FileType.cs
@@ -0,0 +1,11 @@
+﻿namespace CodeskLibrary.Models
+{
+    public class FileType
+    {
+        public int FileTypeId { get; set; }
+
+        public string FileTypeName { get; set; }
+
+        public string FileTypeExtension { get; set; }
+    }
+}
